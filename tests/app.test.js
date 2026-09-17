a983892b1629b8768a4932baef0a0e4d0ab9c897
F9C <ehiga@f9c.com.br>
2026-09-17 12:53:47 -0300

    test: add application test

---
+++ tests/app.test.js
@@ -0,0 +1,13 @@
+import test from "node:test";
+import assert from "node:assert/strict";
+
+import { getStatus } from "../public/app.js";
+
+test("F9C application should be online", () => {
+
+    assert.equal(
+        getStatus(),
+        "F9C CI/CD ONLINE"
+    );
+
+});
